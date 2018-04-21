e054c2675d845dea5ea5c1d3b6a0972ec1827c53
Matthew Miller <mnmiller1@me.com>
2018-04-21 11:41:09 +1000

    Added API stubs

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
 from flask import Flask
-from .tinderfortanda import load
+from .tinderfortanda import (
+    load, get_next_person, get_next_venue, swipe_right, swipe_left
+)
 
 app = Flask(__name__)
+
+
+@app.route('/get_next_card/:card_type', methods=["GET"])
+def get_next_card(card_type=None):
+    if card_type == 'user':
+        return get_next_person()
+    else:
+        return get_next_venue()
+
+
+@app.route('/swipe/:direction/:id')
+def swipe(direction=None, id=None):
+    if direction == 'right':
+        return swipe_right(id)
+    else:
+        return swipe_left(id)
 
 
 if __name__ == "__main__":
--- tinderfortanda/__init__.py
+++ tinderfortanda/__init__.py
@@ -1,1 +1,3 @@
-from .tinderfortanda import load
+from .tinderfortanda import (
+    load, get_next_person, get_next_venue, swipe_left, swipe_right
+)
