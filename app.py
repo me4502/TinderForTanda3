a70356293d9db4aa8f094a98771c7712abdfc618
Matthew Miller <mnmiller1@me.com>
2018-04-21 14:39:52 +1000

    Added js route

--- app.py
+++ app.py
@@ -10,29 +10,41 @@ app = Flask(__name__)
 def index():
     return send_from_directory('client', 'signup.html')
 
+
+@app.route('/js/:filename', methods=['GET'])
+def index(filename=None):
+    return send_from_directory('js', filename)
+
+
 @app.route('/search/jobs', methods=['GET'])
 def job_search():
     return send_from_directory('client', 'job_search.html')
+
 
 @app.route('/search/candidates', methods=['GET'])
 def candidate_search():
     return send_from_directory('client', 'candidate_search.html')
 
+
 @app.route('/profile/employee', methods=['GET'])
 def profileEmployee():
     return send_from_directory('client', 'profileEmployee.html')
+
 
 @app.route('/profile/employer', methods=['GET'])
 def profileEmployer():
     return send_from_directory('client', 'profileEmployer.html')
 
+
 @app.route('/signup', methods=['GET'])
 def signup():
     return send_from_directory('client', 'signup.html')
 
+
 @app.route('/signup/employer', methods=['GET'])
 def signupEmployer():
     return send_from_directory('client', 'signupEmployer.html')
+
 
 @app.route('/get_next_card/:card_type', methods=["GET"])
 def get_next_card(card_type=None):
@@ -48,6 +60,7 @@ def swipe(direction=None, id=None):
         return swipe_right(id)
     else:
         return swipe_left(id)
+
 
 if __name__ == "__main__":
     load()
